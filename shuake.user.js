55df9b8bb6237651af0a807fc98a0abe2a4046f5
gjz010 <gjz010944@gmail.com>
2016-12-19 11:52:27 +0800

    Update shuake.user.js
    removed dependency on localStorage

--- shuake.user.js
+++ shuake.user.js
@@ -5,7 +5,6 @@
 // @version     1
 // @grant       none
 // ==/UserScript==
-//alert("b");
 //自带屏蔽alert和confirm功能
 window.alert=console.log;
 window.confirm=function(str){
@@ -16,20 +15,10 @@ window.confirm=function(str){
 //jQuery.noConflict();
 $(function(){
   console.log("starting...");
-  var storage=window.localStorage;
-  var classes;
-  if(storage["classes"]==undefined){
-    console.log("initializing...");
-    //要改的话自己delete window.localStorage.classes 
-      classes=[
-      {sem:"2016-2017-2",cid:"30240343",sid:"0",done:false}
+  classes=[
+  {sem:"2016-2017-2",cid:"30240343",sid:"0"}
     
-      ];
-  storage["classes"]=JSON.stringify(classes);
-  }else{
-    console.log("old data detected!loading old data...");
-    classes=JSON.parse(storage["classes"]);
-  }
+  ];
   classes.forEach(function(clazz){
     jQuery("#frm_sy").append
     ('<input name="p_rx_id" value="'+clazz.sem+';'+clazz.cid+';'+clazz.sid+';" type="checkbox" checked="checked">');
