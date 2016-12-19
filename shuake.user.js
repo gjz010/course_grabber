7f147f68b197537468f743c2f67cc7cc57007cd3
gjz010 <gjz010944@gmail.com>
2016-12-19 10:29:56 +0800

    Update shuake.user.js
    Great Update
    1.removed jQuery dependency, using the jQuery from the page instead.
    2.multiple-choice!

--- shuake.user.js
+++ shuake.user.js
@@ -2,76 +2,42 @@
 // @name        shuake
 // @namespace   gjz010
 // @include     http://zhjwxk.cic.tsinghua.edu.cn/xkBks.vxkBksXkbBs.do
-// @require     http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js
 // @version     1
 // @grant       none
 // ==/UserScript==
 //alert("b");
-jQuery.noConflict();
-jQuery(function(){
-  //alert("a");
+//自带屏蔽alert和confirm功能
+window.alert=console.log;
+window.confirm=function(str){
+  console.log(str);
+  return true;
+  
+};
+//jQuery.noConflict();
+$(function(){
   console.log("starting...");
   var storage=window.localStorage;
   var classes;
   if(storage["classes"]==undefined){
     console.log("initializing...");
-    classes=/*[
-    {cid:"00690672",sid:"90",done:false},
-    {cid:"00691223",sid:"90",done:false},
-    {cid:"00120181",sid:"90",done:false},
-    {cid:"00140122",sid:"90",done:false},
-    {cid:"00700302",sid:"90",done:false},
-    {cid:"00700852",sid:"90",done:false},
-    {cid:"00680042",sid:"90",done:false},
-    {cid:"01510162",sid:"90",done:false},
-  ];*/
-      classes=[{cid:"04100012",sid:"90",done:false}];
+    //要改的话自己delete window.localStorage.classes 
+      classes=[
+      {sem:"2016-2017-2",cid:"30240343",sid:"0",done:false}
+    
+      ];
   storage["classes"]=JSON.stringify(classes);
   }else{
     console.log("old data detected!loading old data...");
     classes=JSON.parse(storage["classes"]);
   }
-  var cid=jQuery("#p_kch").val();
-  var current;
   classes.forEach(function(clazz){
-    console.log(clazz);
-    if(clazz.cid==cid){
-      if(clazz.done){
-        current=null;
-        alert("该课程已选上！");
-        return;
-      }
-      else{current=clazz;}
-    }
+    jQuery("#frm_sy").append
+    ('<input name="p_rx_id" value="'+clazz.sem+';'+clazz.cid+';'+clazz.sid+';" type="checkbox" checked="checked">');
   });
-  if(current==null) return;
-  if(current==undefined){
-    alert("找不到待选课信息！");
-    return;
-  }
-  console.log(current);
-  var table=jQuery("#table_t");
-  var tbody=jQuery(table.children()[0]);
-  var trs=tbody.children();
-  for(i=0;i<trs.length;i++){
-    tr=jQuery(trs[i]);
-    subid=jQuery(jQuery(tr.children()[2]).children()[0]).html();
-    if(subid==current.sid){
-      remain=jQuery(jQuery(tr.children()[4]).children()[0]).html();
-      if(remain>0){
-        jQuery(jQuery(tr.children()[0]).children()[0]).attr("checked","checked");
-        current.done=true;
-        storage["classes"]=JSON.stringify(classes);
+  window.setTimeout(function(){
         var frm=window.document.frm;
         frm.m.value="saveRxKc";
         frm.submit();
+  },3000);
 
-      }else{
-        console.log("no remain...");
-        window.setTimeout(doQuery,3000);
-        return;
-      }
-    }
-  }
-  alert("找不到待选课信息！");
 });
